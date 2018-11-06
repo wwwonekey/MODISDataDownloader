03e8d087a3b05eb18dca6151340b2a64dc15cb09
wwwonekey <1195041339@qq.com>
2018-11-06 15:00:09 +0800

    修改下载方式

--- NASADataDownloader.py
+++ NASADataDownloader.py
@@ -158,8 +158,8 @@ def DownLoadFiles(urlPath,option,outpath,filename,log):
 
 
 if __name__ == "__main__":
-    yearlist = [2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016]
-    julianDays = [1,1] #第一个为开始日期，第二个为结束日期
-    hvs =[[0,8],[16,9],[32,12]]
+    yearlist = [2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018]
+    julianDays = [1,366] #第一个为开始日期，第二个为结束日期
+    hvs =[[26,5]]
     outpath = "E:\code\python\caa"
-    GetProductData(mainUrl,"MOD13Q1",yearlist,julianDays,hvs,outpath)
+    GetProductData(mainUrl,"MOD11A2",yearlist,julianDays,hvs,outpath)
